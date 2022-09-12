c1b051a09bf54fa5ecedabc15dbcc3156c604f87
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-09-12 22:57:30 +0100

    add data to table

--- data.sql
+++ data.sql
@@ -1,5 +1,10 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+insert into animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (1, 'Agumon', '2020-02-03', 0, TRUE, 10.23);
+insert into animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (3, 'Picachu', '2021-01-07', 1, FALSE, 15.04);
+INSERT INTO animals (name) VALUES ('Charlie')
+VALUES (2, 'Gabumon', '2018-11-15', 2, TRUE, 8.0);
+insert into animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (4, 'Devimon', '2017-05-12', 2, TRUE, 11.0);
