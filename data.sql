e2db25958490053711216793f3e949935453cdc9
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-09-14 11:11:09 +0100

    ADD animals to database

--- data.sql
+++ data.sql
@@ -10,15 +10,15 @@ insert into animals(id, name, date_of_birth, escape_attempts, neutered, weight_k
 VALUES (4, 'Devimon', '2017-05-12', 2, TRUE, 11.0);
 insert into animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES(5, 'Charmande', '2020-02-08', 0, FALSE, -11);
+insert into animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg);
+VALUES(6, 'Squirtle', '1993-04-02', 3, FALSE, -12.13)
 insert into animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES(6, 'Squirtle', '1993-04-02', 3, FALSE, -12.13);
-insert into animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES(6, 'Squirtle', '1993-04-02', 3, FALSE, -12.13);
-insert into animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES(7, 'Plantmon', '2021-11-15', 2, TRUE, -5.7);
+insert into animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg);
 VALUES(8, 'Angemon', '2005-06-12', 1, TRUE, -45);
-insert into animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+insert into animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg);
 VALUES(9, 'Boarmon', '2005-06-07', 7, TRUE, 20.4);
-insert into animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+insert into animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg);
 VALUES(10, 'Blossom', '1998-10-13', 3, TRUE, 17);
 insert into animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES(11, ' Ditto', '2022-05-14', 4, TRUE, 22);
