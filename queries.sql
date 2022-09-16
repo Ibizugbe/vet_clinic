be503878a225c0922690616334303616cf7dca6e
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-09-16 19:56:00 +0100

    add queries

--- queries.sql
+++ queries.sql
@@ -51,3 +51,15 @@ SELECT species.name, COUNT(animals.species_id) FROM animals JOIN species ON spec
 SELECT animals.name from animals JOIN owners ON owners.id = animals.owners_id JOIN species on species.id = animals.species_id WHERE animals.species_id = '2' AND animals.owners_id = '2';
 SELECT animals.name from animals JOIN owners ON owners.id = animals.owners_id WHERE animals.escape_attempts = '0' AND animals.owners_id = '5';
 SELECT full_name, COUNT(owners_id) FROM owners JOIN animals on owners.id = animals.owners_id GROUP BY full_name ORDER BY COUNT (owners_id) desc limit 1;
+
+
+
+select * from animals where id = (select animals_id from visits where vets_id = 1 order by visit_date desc limit 1);
+select count(distinct animals_id) from visits where vets_id = 3;
+select V.name, S.species_id from vets V left join specializations S on V.id = S.vets_id;
+select A.name from animals A join visits V on A.id = V.animals_id where V.vets_id = 3 and V.visit_date between '2020-04-01' and '2020-08-30';
+select A.name, count(V.animals_id) from animals A join visits V on A.id = V.animals_id group by A.name order by count(V.animals_id) desc limit 1;
+select A.name from animals A join visits V on A.id = V.animals_id where V.vets_id = 2 order by V.visit_date asc limit 1;
+select A.name, V.visit_date, V.vets_id from animals A join visits V on a.id = V.animals_id order by V.visit_date desc limit 1;
+select count(*) from visits V join specializations S on V.vets_id = S.vets_id where V.animals_id = S.species_id;
+select S.species_id from specializations S join visits V on S.vets_id = V.vets_id where V.vets_id = 2 group by S.species_id order by count(S.species_id) desc limit 1;
